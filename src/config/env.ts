32d6ebbaeb5596c87cc9fe51a8381bf46bbb8087
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-02-07 11:28:40 -0600

    feat: add env types and validation

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const serverEnvSchema = z.object({
+  NODE_ENV: z.enum(["development", "production", "test"]),
+  TMDB_API_BASE_URL: z.url(),
+  TMDB_API_KEY: z.string(),
+});
+
+const clientEnvSchema = z.object({});
+
+export const serverEnv = serverEnvSchema.parse(process.env);
+
+export const clientEnv = clientEnvSchema.parse(import.meta.env);
---
+++ src/env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="vite/client" />
+
+// Client-side environment variables
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+// Server-side environment variables
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly NODE_ENV: "development" | "production" | "test";
+      readonly TMDB_API_BASE_URL: string;
+      readonly TMDB_API_KEY: string;
+    }
+  }
+}
+
+export {};
