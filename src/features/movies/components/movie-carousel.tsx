3e151d2c8e934fddd8277eaaebc011788c6db28a
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-03-01 19:29:40 -0600

    feat: add movie card link

--- src/features/movies/components/movie-carousel.tsx
+++ src/features/movies/components/movie-carousel.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/carousel";
 import { MovieCard } from "@/features/movies/components/movie-card";
 import type { Movie } from "@/features/movies/types/movie-list";
+import { Link } from "@tanstack/react-router";
 
 interface MovieCarouselProps {
   movies: Movie[];
@@ -23,7 +24,9 @@ export function MovieCarousel({ movies }: MovieCarouselProps) {
       <CarouselContent>
         {movies.map((movie) => (
           <CarouselItem key={movie.id} className="basis-1/2 lg:basis-1/5">
-            <MovieCard movie={movie} />
+            <Link to="/movies/$movieId" params={{ movieId: String(movie.id) }}>
+              <MovieCard movie={movie} />
+            </Link>
           </CarouselItem>
         ))}
       </CarouselContent>
