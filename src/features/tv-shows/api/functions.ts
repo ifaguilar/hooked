4046d500becb038665779af556da83efc17160bd
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-03-08 00:28:45 -0600

    feat: tv shows feature

---
+++ src/features/tv-shows/api/functions.ts
@@ -0,0 +1,37 @@
+import { TvShow } from "@/features/tv-shows/types/model";
+import { tmdbFetch } from "@/lib/tmdb/tmdb-fetch";
+import { TMDBListResponse } from "@/types/tmdb";
+import { TMDBListParamsSchema } from "@/schemas/tmdb";
+import { createServerFn } from "@tanstack/react-start";
+
+export const getPopularTvShows = createServerFn()
+  .inputValidator(TMDBListParamsSchema)
+  .handler(async ({ data }) => {
+    return tmdbFetch<TMDBListResponse<TvShow>>("/3/tv/popular", {
+      params: data,
+    });
+  });
+
+export const getAiringTodayTvShows = createServerFn()
+  .inputValidator(TMDBListParamsSchema)
+  .handler(async ({ data }) => {
+    return tmdbFetch<TMDBListResponse<TvShow>>("/3/tv/airing_today", {
+      params: data,
+    });
+  });
+
+export const getOnTheAirTvShows = createServerFn()
+  .inputValidator(TMDBListParamsSchema)
+  .handler(async ({ data }) => {
+    return tmdbFetch<TMDBListResponse<TvShow>>("/3/tv/on_the_air", {
+      params: data,
+    });
+  });
+
+export const getTopRatedTvShows = createServerFn()
+  .inputValidator(TMDBListParamsSchema)
+  .handler(async ({ data }) => {
+    return tmdbFetch<TMDBListResponse<TvShow>>("/3/tv/top_rated", {
+      params: data,
+    });
+  });
---
+++ src/features/tv-shows/api/queries.ts
@@ -0,0 +1,35 @@
+import {
+  getAiringTodayTvShows,
+  getOnTheAirTvShows,
+  getPopularTvShows,
+  getTopRatedTvShows,
+} from "@/features/tv-shows/api/functions";
+import { queryKeys } from "@/lib/tanstack-query/query-keys";
+import { TMDBListParams } from "@/types/tmdb";
+import { queryOptions } from "@tanstack/react-query";
+
+export const tvShowQueries = {
+  popular: (params: TMDBListParams) =>
+    queryOptions({
+      queryKey: [...queryKeys.POPULAR_TV_SHOWS, params],
+      queryFn: () => getPopularTvShows({ data: params }),
+    }),
+
+  airingToday: (params: TMDBListParams) =>
+    queryOptions({
+      queryKey: [...queryKeys.AIRING_TODAY_TV_SHOWS, params],
+      queryFn: () => getAiringTodayTvShows({ data: params }),
+    }),
+
+  onTheAir: (params: TMDBListParams) =>
+    queryOptions({
+      queryKey: [...queryKeys.ON_THE_AIR_TV_SHOWS, params],
+      queryFn: () => getOnTheAirTvShows({ data: params }),
+    }),
+
+  topRated: (params: TMDBListParams) =>
+    queryOptions({
+      queryKey: [...queryKeys.TOP_RATED_TV_SHOWS, params],
+      queryFn: () => getTopRatedTvShows({ data: params }),
+    }),
+};
