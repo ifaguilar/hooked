e213b50a0d0864cf943bd15c9aaf80e3df90540c
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-03-08 00:29:08 -0600

    feat: improve query keys

--- src/lib/tanstack-query/query-keys.ts
+++ src/lib/tanstack-query/query-keys.ts
@@ -1,6 +1,14 @@
 export const queryKeys = {
-  NOW_PLAYING_MOVIES: "now-playing-movies",
-  POPULAR_MOVIES: "popular-movies",
-  TOP_RATED_MOVIES: "top-rated-movies",
-  UPCOMING_MOVIES: "upcoming-movies",
+  MOVIES: ["movies"],
+  NOW_PLAYING_MOVIES: ["movies", "now-playing"],
+  POPULAR_MOVIES: ["movies", "popular"],
+  TOP_RATED_MOVIES: ["movies", "top-rated"],
+  UPCOMING_MOVIES: ["movies", "upcoming"],
+  TV_SHOWS: ["tv-shows"],
+  AIRING_TODAY_TV_SHOWS: ["tv-shows", "airing-today"],
+  ON_THE_AIR_TV_SHOWS: ["tv-shows", "on-the-air"],
+  POPULAR_TV_SHOWS: ["tv-shows", "popular"],
+  TOP_RATED_TV_SHOWS: ["tv-shows", "top-rated"],
+  PEOPLE: ["people"],
+  POPULAR_PEOPLE: ["people", "popular"],
 } as const;
