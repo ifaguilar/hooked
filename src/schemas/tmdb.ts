bc76d09b38b5e0045ab2ed2ba6e57f360f1fd078
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-05-03 13:48:51 -0600

    fix: tmdb schema

--- src/schemas/tmdb.ts
+++ src/schemas/tmdb.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+import { FIRST_PAGE, MAX_PAGE } from "@/utils/constants";
+
 export const TMDBListParamsSchema = z.object({
-  page: z.number().int().positive().catch(1),
+  page: z.number().int().positive().max(MAX_PAGE).catch(FIRST_PAGE).default(FIRST_PAGE),
 });
