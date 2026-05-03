8dea45b4a1c6f872bc5683cae933f193c6b73f75
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-05-03 13:50:10 -0600

    refactor: move tmdb types

--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -22,3 +22,71 @@ export interface TMDBListResponse<T> {
   total_pages: number;
   total_results: number;
 }
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string | undefined;
+  genre_ids: number[];
+  id: number;
+  original_language: OriginalLanguage;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | undefined;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface TvShow {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: OriginalLanguage;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface KnownFor {
+  adult: boolean;
+  backdrop_path: null | string;
+  id: number;
+  name?: string;
+  original_name?: string;
+  overview: string;
+  poster_path: string;
+  media_type: MediaType;
+  original_language: OriginalLanguage;
+  genre_ids: number[];
+  popularity: number;
+  first_air_date?: Date;
+  vote_average: number;
+  vote_count: number;
+  origin_country?: string[];
+  title?: string;
+  original_title?: string;
+  release_date?: Date;
+  video?: boolean;
+}
+
+export interface Person {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: KnownForDepartment;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  known_for: KnownFor[];
+}
